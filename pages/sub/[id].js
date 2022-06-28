d2afc43926838d28a177b4d37929f6dc8cdfdcb2
Lee <dltmdgns0508@gmail.com>
2022-06-28 14:15:54 +0900

    api와 서버사이드랜더링, 클라이언트 사이드 랜더링 해보기

--- pages/sub/[id].js
+++ pages/sub/[id].js
@@ -1,7 +1,6 @@
-import useRouter from 'next/router'
-
+import {useRouter} from 'next/router';
 export default function Topic(){
-  const router = useRouter();
-  const id = router.query.id;
-  return <>/sub/{id}.js</>
+    const router = useRouter();
+    const id = router.query.id;
+    return <>/sub/{id}.js </>
 }
--- pages/sub/index.js
+++ pages/sub/index.js
@@ -1,3 +1,23 @@
-export default function Index(){
-  return <>/sub/index.js</>
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>index</h1>
+      <ul>
+        <li>
+          <Link href="/sub">/sub/index.js</Link> 
+        </li>
+        <li>
+          <Link href="/sub/about">/sub/about.js</Link> 
+        </li>
+        <li>
+          <Link href="/sub/1">/sub/1.js</Link> 
+        </li>
+        <li>
+          <Link href="/sub/2">/sub/2.js</Link> 
+        </li>
+      </ul>
+    </div>
+  );
 }
